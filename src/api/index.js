58c5f01cf81c89dc15c77d75dd9bab8ad5d86a99
BitLee-design <71900108+BitLee-design@users.noreply.github.com>
2022-10-07 20:52:14 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,28 @@
 import axios from "axios";
 
-const URL = ''
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+const options = {
+  params: {
+    bl_latitude: 'sw.lat',
+    tr_latitude: 'ne.lat',
+    bl_latitude: 'sw.lng',
+    tr_latitude: 'ne.lng',
+  },
+
+  headers: {
+    'X-RapidAPI-Key': '697d742ac1msh6302d389e5bc879p1c0a4ejsn8a332421197c',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
 
 export const getPlacesData = async (sw, ne) => {
     try {
-        const { data: { data }} = await axios.get(URL, {
-            params: [
-                bl_latitude= 'sw.lat',
-                tr_latitude= 'ne.lat',
-                bl_latitude= 'sw.lng',
-                tr_latitude= 'ne.lng',
-            ],
-            headers: {
-                'x-rapidapi-key': '',
-                'x-rapidapi-host': ''
-        
-            }
-        });
+        const { data: { data }} = await axios.get(URL, options); 
 
         return data;
     } catch (error) {
         console.log(error)
     }
 }
+
